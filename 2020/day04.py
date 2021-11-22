0638aaa7a8b0c36e1523e29e846f4c3111733ceb
‘markhend’ <markhend@gmail.com>
2021-11-21 22:16:26 -0600

    Update

---
+++ 2020/day04.py
@@ -0,0 +1,19 @@
+# These are passport keys. A valid passport must have all keys.
+# cid is optional.
+
+keys = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+with open("inp04.txt", 'r') as f:
+    not_seen = keys.copy()
+    cnt = 0  # num valid
+    for line in f:
+        line = line.strip()
+        if len(line) == 0:  # blank line
+            if len(not_seen) == 0:
+                cnt += 1
+            not_seen = keys.copy()
+        else:
+            for kv in line.split():
+                not_seen.discard(kv[:3])
+
+    print(cnt)
