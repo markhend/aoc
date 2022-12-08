4e733ff80487428509d1c89951ffc55fa9112f23
‘markhend’ <markhend@gmail.com>
2022-12-08 17:32:49 -0600

    2022 day07 update

--- 2022/day07/a.py
+++ 2022/day07/a.py
@@ -1,18 +1,18 @@
-with open("test.txt", "r") as f:
+with open("in.txt", "r") as f:
     f = [l.strip() for l in f.readlines()]
 
 
 class Dir:
-    def __init__(self, name, parent, dirs=[], files=set()):
+    def __init__(self, name, parent):
         self.name = name
         self.parent = parent
-        self.dirs = dirs
-        self.files = files
+        self.dirs = []
+        self.files = set()
 
     def get_size(self):
         res = sum(f.size for f in self.files)
         for dir in self.dirs:
-            print(dir.get_size())
+            res += dir.get_size()
         return res
 
 
@@ -22,40 +22,86 @@ class File:
         self.size = size
 
 
-# each dir has a set of sub directory names and list of files which are
-# tuples.  [{dirs} [(size, name)]]
-# keep track of curr_dir for 'cd ..' command
-
 root = Dir("root", None)
 curr = root
 
 for line in f[1:]:  # skip first line: $ cd /
-    print(f"{line=}")
-    if line == '$ cd ..':  # $ cd ..
+    # print(f"{line=}")
+
+    if line[:7] == '$ cd ..':  # $ cd ..
         curr = curr.parent
-
-    elif line[:3] == 'dir':  # dir dir_name
-        _, dir_name = line.split()
-        if not any(dir.name == dir_name for dir in curr.dirs):
-            curr.dirs.append(Dir(dir_name, curr))
-            print(f"{curr.dirs}")
-
-    elif line[:4] == '$ cd':  # $ cd dir_name
-        _, _, dir_name = line.split()
-        # we can assume dir exists
-        for dir in curr.dirs:
-            if dir.name == dir_name:
-                curr = dir
 
     elif line == '$ ls':  # $ ls
         continue
 
+    elif line[:3] == 'dir':  # dir dir_name
+        continue
+
+    elif line[:4] == '$ cd':  # $ cd dir_name
+        _, _, dir_name = line.split()
+        if not any(dir.name == dir_name for dir in curr.dirs):
+            tmp = Dir(dir_name, curr)
+            curr.dirs.append(tmp)
+            curr = tmp
+        else:  # change curr to existing directory
+            for dir in curr.dirs:
+                if dir.name == dir_name:
+                    curr = dir
+
     else:  # file_size file_name
         size, name = line.split()
-        file = name.replace('.', 'dot')
+        name = name.replace('.', 'dot')
         size = int(size)
-        print(size, name)
-        curr.files.add(File(name, size))
-        print(f"{curr.files=}")
+        if not any(file.name == name for file in curr.files):
+            curr.files.add(File(name, size))
 
-print(root.get_size())
+
+# find all of the directories with a total size of at most 100000,
+# then calculate the sum of their total sizes
+print("finding dirs under 100k...")
+
+res = []
+
+def get_size_under100k(self):
+    for dir in self.dirs:
+        tmp = dir.get_size()
+        # print("in dir", dir.name, "size is", tmp)
+        if tmp <= 100000:
+            # print(dir.name, tmp)
+            res.append(tmp)
+        get_size_under100k(dir)
+
+
+get_size_under100k(root)
+print(res)
+print(f"{sum(res)=}")  # 1908462
+print()
+
+# Part B:
+# Total system disk space = 70000000
+# Needed to run update = 30000000
+# Find the smallest directory that, if deleted, would free up enough 
+# space on the filesystem to run the update. What is the total size of 
+# that directory?
+
+root_size = root.get_size()
+print(f"{root_size=}")
+# for file in root.files:
+#     print(file.name)
+space_free = 70000000 - root_size
+space_needed = 30000000 - space_free
+print(f"{space_needed=}")
+res = []
+
+def find_dir_to_free(self):
+    for dir in self.dirs:
+        tmp = dir.get_size()
+        # print(tmp, dir.name)
+        if tmp >= space_needed:
+            res.append((tmp, dir.name))
+        find_dir_to_free(dir)
+
+
+find_dir_to_free(root)
+print(res)
+print(f"{min(res)=}")
