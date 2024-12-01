bd2824b5b461d8eabeed29ce19398f5a11428608
‘markhend’ <markhend@gmail.com>
2024-12-01 11:34:31 -0600

    update

--- 2023/day01/Program.cs
+++ 2023/day01/Program.cs
@@ -105,27 +105,17 @@ int LastWordIdx(string line)
 
 char WordToNum(string s)
 {
-    switch (s)
+    return s switch
     {
-        case "one":
-            return '1';
-        case "two":
-            return '2';
-        case "three":
-            return '3';
-        case "four":
-            return '4';
-        case "five":
-            return '5';
-        case "six":
-            return '6';
-        case "seven":
-            return '7';
-        case "eight":
-            return '8';
-        case "nine":
-            return '9';
-        default:
-            return '0';
-    }
+        "one" => '1',
+        "two" => '2',
+        "three" => '3',
+        "four" => '4',
+        "five" => '5',
+        "six" => '6',
+        "seven" => '7',
+        "eight" => '8',
+        "nine" => '9',
+        _ => '0',
+    };
 }
